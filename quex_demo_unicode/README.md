b28d3cccf95b81c989355deaa353404504ebcbd7
mivan <ivan.mittelholcz@gmail.com>
2015-09-24 13:15:54 +0200

    unicode demo

---
+++ quex_demo_unicode/README.md
@@ -0,0 +1,6 @@
+Demo utf8-as input feldolgozására uicode character properties-en alapuló
+szabályokkal.
+
+* input: fájlból olvas (utf8 text)
+* output: stdout (xml)
+
---
+++ quex_demo_unicode/main.cpp
@@ -0,0 +1,37 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<sstream>
+
+#include "chr_chr_lexer"
+
+
+
+
+
+int main(int argc, char** argv)
+{
+
+    chr::Token*       token_p = 0x0;
+    // chr::chr_lexer    lexer("input.txt"); ez is fordul, de rossz kimenetet ad
+    FILE* fh = fopen("input.txt", "rb");
+    chr::chr_lexer lexer(fh, "UTF8");
+
+    for (lexer.receive(&token_p); token_p->type_id() != CHR_TERMINATION; lexer.receive(&token_p))
+    {
+        // nyitó tag
+        std::cout << "<" << token_p->type_id_name() << ">";
+        // token
+        std::cout << token_p->pretty_char_text();
+        // záró tag
+        std::cout << "</" << token_p->type_id_name() << ">" << std::endl;
+
+        /* std::cout << "full: " << std::string(*token_p) << std::endl; */
+        /* std::cout << "id:   " << token_p->type_id() << std::endl; */
+        /* std::cout << "type: " << token_p->type_id_name() << std::endl; */
+        /* std::cout << "text: '" << token_p->pretty_char_text() << "'\n\n"; */
+    }
+
+    return 0;
+}
+
