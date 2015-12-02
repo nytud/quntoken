5ad4111eb564a83575737725937c96ffc7bab63f
mivan <ivan.mittelholcz@gmail.com>
2015-12-02 10:48:51 +0100

    huntoken fele mondatrabonto atirva quex-re

---
+++ quex_sentencizer/main.cpp
@@ -0,0 +1,26 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<sstream>
+
+#include "snt_snt_lexer"
+
+
+int main(int argc, char** argv)
+{
+
+    snt::Token*       token_p = 0x0;
+    FILE* fh = fopen("input.txt", "rb");
+    snt::snt_lexer lexer(fh, "UTF8");
+
+    for (lexer.receive(&token_p); token_p->type_id() != SNT_TERMINATION; lexer.receive(&token_p))
+    {
+        // csak a token szövegét használjuk fel, a típus nem érdekes (a szöveg
+        // már a quex-ben dekorálva lett)
+        /* std::cout << token_p->pretty_char_text() << std::endl; */
+        std::cout << token_p->type_id_name() << "\t" << token_p->pretty_char_text() << std::endl;
+    }
+
+    return 0;
+}
+
