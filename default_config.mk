b2ed0a24363525ccf1ee76353dfc189d416c2ebb
mivan <ivan.mittelholcz@gmail.com>
2016-03-16 09:46:07 +0100

    restructure basic token rules

--- default_config.mk
+++ default_config.mk
@@ -23,6 +23,7 @@ TEST_FILES = $(MYTEST_DIR)/test_default_prep_invalid.txt \
              $(MYTEST_DIR)/test_default_snt_newline.txt  \
              $(MYTEST_DIR)/test_default_sntcorr_all.txt \
              $(MYTEST_DIR)/test_default_sntcorr_luth.txt \
-             $(MYTEST_DIR)/test_default_token_dotsandpars.txt \
-             $(MYTEST_DIR)/test_default_token_hyphenanddash.txt \
+             $(MYTEST_DIR)/test_default_token_dots.txt \
+             $(MYTEST_DIR)/test_default_token_parenthesis.txt \
+             $(MYTEST_DIR)/test_default_token_hyphen.txt \
 
