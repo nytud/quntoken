5d18dc09d0dde1f9922730abd7304e9d56c168c5
m-ivan <ivan.mittelholcz@gmail.com>
2016-04-05 22:44:40 +0200

    argumet parsing: -f format (json, xml)

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,28 +3,71 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <map>
+#include <unistd.h>
 #include "quntoken_api.h"
 
 
-int main(int argc, char** argv)
-{
+std::map<std::string, OUTPUT_TYPE> create_format_map();
+
+
+int main(int argc, char** argv) {
+
+    // commandline arguments
+    int c;
+    int format_flag = 0;
+    std::string format;
+    while( (c = getopt(argc, argv, "f:")) != -1 ) {
+        switch (c) {
+            case 'f':
+                format_flag = 1;
+                format = optarg;
+                break;
+            default:
+                exit(1);
+                break;
+        }
+    }
+
     // input file
-    if (argc < 2)
-    {
+    char* input_file;
+    if(optind < argc) {
+        input_file = argv[optind];
+    }
+    else {
         std::cerr << "Missing file name!" << std::endl;
         exit(0);
     }
 
-    // create input stringstream from file:
-    std::ifstream in(argv[1]);
-    std::stringstream fst_input;
-    fst_input << in.rdbuf();
+    // output format
+    std::map<std::string, OUTPUT_TYPE> format_map = create_format_map();
+    OUTPUT_TYPE out_type;
+    try {
+        out_type = (format_flag ? format_map.at(format) : XML );
+    }
+    catch(const std::out_of_range & err) {
+        std::cerr << "Wrong format. Valid formats: xml, json" << std::endl;
+        exit(1);
+    }
 
-    quntoken_print({PREP, SNT, SNTCORR, SNTCORR, TOKEN}, &fst_input, XML);
-    /* quntoken_print({PREP, SNT, SNTCORR, SNTCORR, TOKEN}, &fst_input, JSON); */
+    // call quntoken
+    std::ifstream inp_fstream(input_file);
+    std::stringstream inp_sstream;
+    inp_sstream << inp_fstream.rdbuf();
+
+    quntoken_print({PREP, SNT, SNTCORR, SNTCORR, TOKEN}, &inp_sstream, out_type);
 
     std::cout << std::endl;
 
     return 0;
+}
+
+
+
+std::map<std::string, OUTPUT_TYPE> create_format_map() {
+    std::map<std::string, OUTPUT_TYPE> format_map;
+    format_map["json"] = JSON;
+    format_map["xml"] = XML;
+    return format_map;
 }
 
